9b3368adf0b6b09ad35ad7279b7a5bb4be72ce7d
gqueico <gqueico@gmail.com>
2024-10-26 16:19:41 -0300

    :lipstick: Create style for the remove class

--- scripts.js
+++ scripts.js
@@ -82,7 +82,7 @@ const DOM = {
       <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-        <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
+        <img class="remove" onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
       </td>
     `
     return html
